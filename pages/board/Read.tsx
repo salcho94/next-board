9a7ed9ede56e12db3f81160ac2f9121d3a09ddc7
kjs <salcho94@naver.com>
2024-06-23 14:31:08 +0900

    board

--- pages/board/Read.tsx
+++ pages/board/Read.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Board } from "../constants/board";
+import { Board } from "../../constants/board";
 import dayjs from "dayjs";
 
 const Read = () => {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
 // pages/index.tsx
 import Link from "next/link";
-import Read from "./Read";
+import Read from "./board/Read";
 
 export default function Home() {
   return (
     <div>
       <h1>뽀동이의 게시판</h1>
       <Read />
-      <Link href={"./create"}>글쓰기</Link>
+      <Link href={"./board/create"}>글쓰기</Link>
     </div>
   );
 }
